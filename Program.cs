b1274437e250e4f5a51491768be655e2a39b7f80
caitlinlamb <caitlinlamb2007@gmail.com>
2025-09-30 12:03:01 +0100

    fuelling links vehicles to pumps using matrix

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@
 
         public static Timer create_car = new Timer(1500);
         public static Queue<object> vehicle_queue = new Queue<object>();
+        
 
         public static Timer fuelling_timer = new Timer(8000);
 
@@ -23,6 +24,94 @@
         public static Pump p7 = new Pump();
         public static Pump p8 = new Pump();
         public static Pump p9 = new Pump();
+
+        //matrix
+        public static Object[,] pump_status = new Object[9, 2]
+        {
+            {p1, 0},
+            {p2, 0},
+            {p3, 0},
+            {p4, 0},
+            {p5, 0},
+            {p6, 0},
+            {p7, 0},
+            {p8, 0},
+            {p9, 0}
+        };
+
+        internal class Pump
+        {
+
+            private string status = "avail"; // field
+            public string Status   // property
+            {
+                get { return status; }
+                set { status = value; }
+            }
+
+            public void fuel(object vehicle, object pump)
+            {
+                status = "busy ";
+                fuelling_timer.Start();
+                //removes object from end of vehicle waiting queue and adds it to currenyl fuelling list. need to ammend to make it alignwith pump number
+                if (pump == p1)
+                {
+                    pump_status[0, 1] = vehicle;
+                    pump_status[0, 0] = pump;
+                }
+                else if (pump == p2)
+                {
+                    pump_status[1, 1] = vehicle;
+                    pump_status[1, 0] = pump;
+                }
+                else if (pump == p3)
+                {
+                    pump_status[2, 1] = vehicle;
+                    pump_status[2, 0] = pump;
+                }
+                else if (pump == p4)
+                {
+                    pump_status[3, 1] = vehicle;
+                    pump_status[3, 0] = pump;
+                }
+                else if (pump == p5)
+                {
+                    pump_status[4, 1] = vehicle;
+                    pump_status[4, 0] = pump;
+                }
+                else if (pump == p6)
+                {
+                    pump_status[5, 1] = vehicle;
+                    pump_status[5, 0] = pump;
+                }
+                else if (pump == p7)
+                {
+                    pump_status[6, 1] = vehicle;
+                    pump_status[6, 0] = pump;
+                }
+                else if (pump == p8)
+                {
+                    pump_status[7, 1] = vehicle;
+                    pump_status[7, 0] = pump;
+                }
+                else if (pump == p9)
+                {
+                    pump_status[8, 1] = vehicle;
+                    pump_status[8, 0] = pump;
+                }
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
 
         public static void console_text()
         {
@@ -55,57 +144,57 @@
 
                 if (pump.Key == ConsoleKey.D1)
                 {
-                    vehicle_queue.Dequeue();
-                    p1.fuel();
+                    p1.fuel(vehicle_queue.Dequeue(), p1);
+                    
 
 
                 }
                 else if (pump.Key == ConsoleKey.D2)
                 {
                     vehicle_queue.Dequeue();
-                    p2.fuel();
+                    p2.fuel(vehicle_queue.Dequeue(), p2);
 
                 }
                 else if (pump.Key == ConsoleKey.D3)
                 {
                     vehicle_queue.Dequeue();
-                    p3.fuel();
+                    p3.fuel(vehicle_queue.Dequeue(), p3);
 
                 }
                 else if (pump.Key == ConsoleKey.D4)
                 {
                     vehicle_queue.Dequeue();
-                    p4.fuel();
+                    p4.fuel(vehicle_queue.Dequeue(), p4);
 
                 }
                 else if (pump.Key == ConsoleKey.D5)
                 {
                     vehicle_queue.Dequeue();
-                    p5.fuel();
+                    p5.fuel(vehicle_queue.Dequeue(), p5);
 
                 }
                 else if (pump.Key == ConsoleKey.D6)
                 {
                     vehicle_queue.Dequeue();
-                    p6.fuel();
+                    p6.fuel(vehicle_queue.Dequeue(), p6);
 
                 }
                 else if (pump.Key == ConsoleKey.D7)
                 {
                     vehicle_queue.Dequeue();
-                    p7.fuel();
+                    p7.fuel(vehicle_queue.Dequeue(), p7);
 
                 }
                 else if (pump.Key == ConsoleKey.D8)
                 {
                     vehicle_queue.Dequeue();
-                    p8.fuel();
+                    p8.fuel(vehicle_queue.Dequeue(), p8);
 
                 }
                 else if (pump.Key == ConsoleKey.D9)
                 {
                     vehicle_queue.Dequeue();
-                    p9.fuel();
+                    p9.fuel(vehicle_queue.Dequeue(), p9);
 
                 }
 
@@ -124,11 +213,21 @@
             create_car.Start();
             create_car.AutoReset = true;
 
+            fuelling_timer.Elapsed += Fuelling_timer_Elapsed; ;
+            create_car.Enabled = true;
+
             Thread console_update = new Thread(console_text);
             console_update.Start();
 
             Thread key_detector = new Thread(detect_key);
             key_detector.Start();
+
+
+        }
+
+        private static void Fuelling_timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            //dequeue here?
         }
 
         public static void Create_car_Elapsed(object? sender, ElapsedEventArgs e)
--- Pump.cs
+++ Pump.cs
@@ -6,22 +6,9 @@ using System.Threading.Tasks;
 
 namespace programming_fundamentals_assignments
 {
-    internal class Pump
-    {
-        private string status = "avail"; // field
-        public string Status   // property
-        {
-            get { return status; }
-            set { status = value; }
-        }
+    
+    
 
-        public void fuel()
-        {
-            status = "busy ";
-            Console.WriteLine("status = busy");
-        }
-
-    }
 
 }
 
